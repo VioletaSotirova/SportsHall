f3ca5908f4d50801196c005ed6e55cf7836767b5
VioletaSotirova <villy_553@abv.bg>
2024-10-28 16:03:45 +0200

    Create ApplicationConstants.cs

---
+++ SportsHall.Common/ApplicationConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsHall.Common
+{
+    public static class ApplicationConstants
+    {
+        public const int ReleaseYear = 2024;
+    }
+}
