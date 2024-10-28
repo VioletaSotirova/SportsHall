180c911cc3c7fada237250f409d108dcb4cab780
VioletaSotirova <villy_553@abv.bg>
2024-10-28 16:14:27 +0200

    Create Sport.cs

---
+++ SportsHall.Data.Models/Sport.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsHall.Data.Models
+{
+    public class Sport
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public int MaxParticipants { get; set; }
+        public IEnumerable<SportCoach> SportsCoaches { get; set; } = new List<SportCoach>();
+    }
+}
