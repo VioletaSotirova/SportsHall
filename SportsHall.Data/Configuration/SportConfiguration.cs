5a3d6dc529b2183a32050e6c05da0750b795b586
VioletaSotirova <villy_553@abv.bg>
2024-10-28 16:14:41 +0200

    Create SportConfiguration

---
+++ SportsHall.Data/Configuration/SportConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SportsHall.Data.Models;
+using System.Reflection.Emit;
+using static SportsHall.Common.EntityValidationConstants.Sport;
+
+
+namespace SportsHall.Data.Configuration
+{
+    public class SportConfiguration : IEntityTypeConfiguration<Sport>
+    {
+        public void Configure(EntityTypeBuilder<Sport> builder)
+        {
+            builder.HasKey(s => s.Id);
+
+            builder.Property(s => s.Name)
+                .IsRequired()
+                .HasMaxLength(NameMaxLength);
+
+            builder.Property(s => s.Description)
+                .IsRequired()
+                .HasMaxLength(DescriptionMaxLength);
+
+            builder.Property(s => s.MaxParticipants)
+            .IsRequired();
+
+        }
+    }
+}
