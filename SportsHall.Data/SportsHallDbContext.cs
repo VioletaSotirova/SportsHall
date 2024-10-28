0defb944740c7bfedfdb865e64a6d8cb46d68e5e
VioletaSotirova <villy_553@abv.bg>
2024-10-28 16:12:35 +0200

    Create SportsHallDbContext.cs

---
+++ SportsHall.Data/SportsHallDbContext.cs
@@ -0,0 +1,29 @@
+﻿namespace SportsHall.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using SportsHall.Data.Models;
+    using System.Reflection;
+
+    public class SportsHallDbContext : DbContext
+    {
+        public SportsHallDbContext()
+        {
+            
+        }
+
+        public SportsHallDbContext(DbContextOptions options)
+            :base(options) 
+        {
+            
+        }
+
+        public virtual DbSet<Sport> Sports { get; set; } = null!;
+        public virtual DbSet<Coach> Coaches { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
+ 
