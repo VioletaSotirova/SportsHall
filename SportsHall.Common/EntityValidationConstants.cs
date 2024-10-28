5126c32e40621e4243344aebbfb6293e3ebd5b19
VioletaSotirova <villy_553@abv.bg>
2024-10-28 16:05:23 +0200

    Create EntityValidationConstants.cs

---
+++ SportsHall.Common/EntityValidationConstants.cs
@@ -0,0 +1,26 @@
+﻿namespace SportsHall.Common
+{
+    public static class EntityValidationConstants
+    {
+        public static class Sport
+        {
+            public const int NameMinLength = 3;
+            public const int NameMaxLength = 30;
+
+            public const int DescriptionMinLength = 7;
+            public const int DescriptionMaxLength = 300;
+        }
+
+        public static class Coach
+        {
+            public const int FirstNameMaxLength = 20;
+            public const int LastNameMaxLength = 40;
+
+            public const int PhoneMaxLength = 10;
+
+            public const string EmailRegex = "^([\\w\\.\\-]+)@([\\w\\-]+)((\\.(\\w){2,3})+)$";
+
+            public const int ExpirienceMaxLength = 300;
+        }
+    }
+}
