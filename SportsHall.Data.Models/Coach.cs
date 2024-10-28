ffbb51b4a58f5e54faa8adc94601b59225ce735e
VioletaSotirova <villy_553@abv.bg>
2024-10-28 16:13:05 +0200

    Create data model Coach

---
+++ SportsHall.Data.Models/Coach.cs
@@ -0,0 +1,14 @@
+﻿namespace SportsHall.Data.Models
+{
+    public class Coach
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? Email { get; set; }
+        public string Phone { get; set; } = null!;
+        public string Expirience { get; set; } = null!;
+        public IEnumerable<SportCoach> SportsCoaches { get; set; } = new List<SportCoach>();
+
+    }
+}
