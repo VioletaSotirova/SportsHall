8cf81d1e1a12d438932fac4207ffa18c5f0e961a
VioletaSotirova <villy_553@abv.bg>
2024-10-28 16:13:27 +0200

    Create CoachConfiguration.cs

---
+++ SportsHall.Data/Configuration/CoachConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SportsHall.Data.Models;
+using static SportsHall.Common.EntityValidationConstants.Coach;
+
+namespace SportsHall.Data.Configuration
+{
+    public class CoachConfiguration : IEntityTypeConfiguration<Coach>
+    {
+        public void Configure (EntityTypeBuilder<Coach> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.FirstName)
+                .IsRequired()
+                .HasMaxLength(FirstNameMaxLength);
+
+            builder.Property(c => c.LastName)
+                .IsRequired()
+                .HasMaxLength(LastNameMaxLength);
+
+            builder.Property(c => c.Phone)
+               .IsRequired()
+               .HasMaxLength(PhoneMaxLength);
+
+            builder.Property(c => c.Expirience)
+               .IsRequired()
+               .HasMaxLength(ExpirienceMaxLength);
+        }
+    }
+}
