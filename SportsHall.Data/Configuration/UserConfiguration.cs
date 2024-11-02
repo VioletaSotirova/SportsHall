3efd792ec363a829ffb5b3e98596dadbc722ba1f
VioletaSotirova <villy_553@abv.bg>
2024-11-02 19:04:33 +0200

    Create UserConfiguration.cs

---
+++ SportsHall.Data/Configuration/UserConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SportsHall.Data.Models;
+using static SportsHall.Common.EntityValidationConstants.User;
+
+
+namespace SportsHall.Data.Configuration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u => u.Id);
+
+            builder.Property(u => u.FirstName)
+                .IsRequired()
+                .HasMaxLength(FirstNameMaxLength);
+
+            builder.Property(u => u.LastName)
+               .IsRequired()
+               .HasMaxLength(LastNameMaxLength);
+
+            builder.Property(u => u.Phone)
+                .IsRequired()
+                .HasMaxLength(PhoneMaxLength);
+        }
+    }
+}
