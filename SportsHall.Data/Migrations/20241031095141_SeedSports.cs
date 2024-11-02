030e13c97999d26e15ebdbc869bffc4240bc45cf
VioletaSotirova <villy_553@abv.bg>
2024-11-02 19:06:57 +0200

    Added Database Migrations

---
+++ SportsHall.Data/Migrations/20241031095141_SeedSports.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace SportsHall.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedSports : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Sports",
+                columns: new[] { "Id", "Description", "ImageUrl", "MaxParticipants", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Zumba is a Latin-inspired dance workout that instructors say is primarily an aerobic workout — and it's all about having fun. Few exercise classes have had Zumba's staying power.", null, 30, "Zumba" },
+                    { 2, "Indoor cycling, often called spinning, is a form of exercise with classes focusing on endurance, strength, intervals, high intensity (race days) and recovery, and involves using a special stationary exercise bicycle with a weighted flywheel in a classroom setting.", null, 20, "Spinning" },
+                    { 3, "Yoga as exercise is a physical activity consisting mainly of postures, often connected by flowing sequences, sometimes accompanied by breathing exercises, and frequently ending with relaxation lying down or meditation.", null, 5, "Yoga" },
+                    { 4, "CrossFit is focused on constantly varied high-intensity functional movement, drawing on categories and exercises such as calisthenics, Olympic-style weightlifting, powerlifting, strongman-type events, plyometrics, bodyweight exercises, indoor rowing, aerobic exercise, running, and swimming.", null, 14, "CrossFit" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Sports",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Sports",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Sports",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Sports",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
---
+++ SportsHall.Data/Migrations/20241101192432_SeedTrainingAndCoaches.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace SportsHall.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedTrainingAndCoaches : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Trainings",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.InsertData(
+                table: "SportsCoaches",
+                columns: new[] { "CoachId", "SportId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 1, 2 },
+                    { 2, 4 },
+                    { 3, 3 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Trainings",
+                columns: new[] { "Id", "AvailableSpot", "CoachId", "Duration", "Location", "SportId", "Start", "Status" },
+                values: new object[,]
+                {
+                    { 1, 30, 1, 50, "Plovdiv, bul.Hristo Botev 4, DynamicFitCenter, Hall 3", 1, new DateTime(2024, 10, 31, 8, 30, 0, 0, DateTimeKind.Unspecified), false },
+                    { 2, 20, 1, 30, "Plovdiv, bul.Hristo Botev 4, DynamicFitCenter, Hall 1", 2, new DateTime(2024, 10, 31, 10, 30, 0, 0, DateTimeKind.Unspecified), false },
+                    { 3, 5, 3, 50, "Plovdiv, bul.Hristo Botev 4, DynamicFitCenter, Hall 5", 3, new DateTime(2024, 10, 31, 11, 0, 0, 0, DateTimeKind.Unspecified), false },
+                    { 4, 14, 2, 45, "Plovdiv, bul.Hristo Botev 4, DynamicFitCenter, Hall 5", 4, new DateTime(2024, 10, 31, 11, 50, 0, 0, DateTimeKind.Unspecified), false }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "SportsCoaches",
+                keyColumns: new[] { "CoachId", "SportId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "SportsCoaches",
+                keyColumns: new[] { "CoachId", "SportId" },
+                keyValues: new object[] { 1, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "SportsCoaches",
+                keyColumns: new[] { "CoachId", "SportId" },
+                keyValues: new object[] { 2, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "SportsCoaches",
+                keyColumns: new[] { "CoachId", "SportId" },
+                keyValues: new object[] { 3, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "Trainings",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Trainings",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Trainings",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Trainings",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Trainings",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150);
+        }
+    }
+}
