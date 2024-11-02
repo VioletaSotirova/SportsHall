574e51fe91a5b3303131b362e606933764c128dd
VioletaSotirova <villy_553@abv.bg>
2024-11-02 19:09:48 +0200

    Create SportController.cs

---
+++ SportsHall.Web/Controllers/SportController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SportsHall.Data;
+using SportsHall.Web.ViewModels;
+
+namespace SportsHall.Web.Controllers
+{
+    public class SportController : Controller
+    {
+        private readonly SportsHallDbContext context;
+        public SportController(SportsHallDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var sports = await this.context
+                .Sports
+                .AsNoTracking()
+                .Select(s => new SportsViewModel
+                {
+                    Id = s.Id,
+                    Name = s.Name,
+                    Image = s.ImageUrl
+                })
+                .ToListAsync();
+                
+            return View(sports);
+        }
+
+    }
+}
