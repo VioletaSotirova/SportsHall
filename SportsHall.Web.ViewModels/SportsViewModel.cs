635860489ee334bf0d95de88fbb4a27cad08c5b2
VioletaSotirova <villy_553@abv.bg>
2024-11-02 19:11:37 +0200

    Create SportsViewModel.cs

---
+++ SportsHall.Web.ViewModels/SportsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsHall.Web.ViewModels
+{
+    public class SportsViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string? Image { get; set; }
+    }
+}
