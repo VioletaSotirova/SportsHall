e5bd841a2d459684aa1838e842e24604ae58912f
VioletaSotirova <villy_553@abv.bg>
2024-11-05 22:51:26 +0200

    Update Reservation.cs

--- SportsHall.Data.Models/Reservation.cs
+++ SportsHall.Data.Models/Reservation.cs
@@ -1,13 +1,15 @@
 ﻿
+using Microsoft.AspNetCore.Identity;
+
 namespace SportsHall.Data.Models
 {
     public class Reservation
     {
         public int Id { get; set; }
         public int TrainingId { get; set; }
-        public Training Training { get; set; }
+        public Training Training { get; set; } = null!;
         public int UserId { get; set; }
-        public User User { get; set; }
+        public ApplicationUser ApplicationUser { get; set; } = null!;
         public DateTime CreatedOn { get; set; }
     }
 }
