b7a444f185331073fa5456221cbcaf236fd3ebb9
VioletaSotirova <villy_553@abv.bg>
2024-11-05 21:58:27 +0200

    Create ApplicationUser.cs

---
+++ SportsHall.Data.Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SportsHall.Data.Models
+{
+    public class ApplicationUser : IdentityUser<int>
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public IEnumerable<Reservation> Reservations { get; set; } = new List<Reservation>();
+    }
+}
