3bc78a69190576e1c599ccb123bf814b1c6bff17
VioletaSotirova <villy_553@abv.bg>
2024-11-05 22:51:06 +0200

    Create ApplicationUserConfiguration.cs

---
+++ SportsHall.Data/Configuration/ApplicationUserConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SportsHall.Data.Models;
+using static SportsHall.Common.EntityValidationConstants.ApplicationUser;
+
+
+namespace SportsHall.Data.Configuration
+{
+    public class ApplicationUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+
+            builder.Property(au => au.FirstName)
+                .IsRequired()
+                .HasMaxLength(FirstNameMaxLength);
+
+            builder.Property(au => au.LastName)
+               .IsRequired()
+               .HasMaxLength(LastNameMaxLength);
+
+        }
+    }
+}
