4f60f9b1a45c80048326a306e94f0396e0260003
VioletaSotirova <villy_553@abv.bg>
2024-11-11 16:03:41 +0200

    Update SportController

--- SportsHall.Web/Controllers/SportController.cs
+++ SportsHall.Web/Controllers/SportController.cs
@@ -1,31 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+
 using SportsHall.Data;
 using SportsHall.Web.ViewModels;
+using SportsHall.Services.Data.Interfaces;
 
 namespace SportsHall.Web.Controllers
 {
     public class SportController : Controller
     {
         private readonly SportsHallDbContext context;
-        public SportController(SportsHallDbContext context)
+        private readonly ISportService sportService;
+        public SportController(SportsHallDbContext context, ISportService sportService)
         {
             this.context = context;
+            this.sportService = sportService;
         }
 
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var sports = await this.context
-                .Sports
-                .AsNoTracking()
-                .Select(s => new SportsViewModel
-                {
-                    Id = s.Id,
-                    Name = s.Name,
-                    Image = s.ImageUrl
-                })
-                .ToListAsync();
+            IEnumerable<SportsViewModel> sports =
+                await this.sportService.GetAllAsync();
                 
             return View(sports);
         }
