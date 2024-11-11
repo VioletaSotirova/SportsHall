06e3742fde1e62f8ccde305fd3a1bdb4ecd3d0c8
VioletaSotirova <villy_553@abv.bg>
2024-11-11 15:57:12 +0200

    Create IRepository and BaseRepository

---
+++ SportsHall.Data/Repository/BaseRepository.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsHall.Data.Repository.Interfaces;
+using System.Formats.Tar;
+
+namespace SportsHall.Data.Repository
+{
+    public class BaseRepository<TType, TId> : IRepository<TType, TId>
+        where TType : class
+    {
+        private readonly SportsHallDbContext dbContext;
+        private readonly DbSet<TType> dbSet;
+        public BaseRepository(SportsHallDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            this.dbSet = this.dbContext.Set<TType>();
+        }
+        public void Add(TType item)
+        {
+            this.dbSet.Add(item);
+            this.dbContext.SaveChanges();
+        }
+
+        public async Task AddAsync(TType item)
+        {
+            await this.dbSet.AddAsync(item);
+            await this.dbContext.SaveChangesAsync();
+        }
+
+        public bool Delete(TType entity)
+        {
+            this.dbSet.Remove(entity);
+            this.dbContext.SaveChanges();
+
+            return true;
+        }
+
+        public async Task<bool> DeleteAsync(TType entity)
+        {
+            this.dbSet.Remove(entity);
+            await this.dbContext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public IEnumerable<TType> GetAll()
+        {
+            return this.dbSet.ToArray();
+        }
+
+        public async Task<IEnumerable<TType>> GetAllAsync()
+        {
+            return await this.dbSet.ToArrayAsync();
+        }
+
+        public IQueryable<TType> GetAllAttached()
+        {
+            return this.dbSet.AsQueryable();
+        }
+
+        public TType GetById(TId id)
+        {
+            TType entity = this.dbSet
+                .Find(id);
+
+            return entity;
+        }
+
+        public async Task<TType> GetByIdAsync(TId id)
+        {
+            TType entity = await this.dbSet
+                .FindAsync(id);
+
+            return entity;
+        }
+        public bool Update(TType item)
+        {
+            try
+            {
+                this.dbSet.Attach(item);
+                this.dbContext.Entry(item).State = EntityState.Modified;
+                this.dbContext.SaveChanges();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> UpdateAsync(TType item)
+        {
+            try
+            {
+                this.dbSet.Attach(item);
+                this.dbContext.Entry(item).State = EntityState.Modified;
+                await this.dbContext.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ SportsHall.Data/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace SportsHall.Data.Repository.Interfaces
+{
+    public interface IRepository<TType, TId>
+    {
+        TType GetById(TId id);
+        Task<TType> GetByIdAsync(TId id);
+        IEnumerable<TType> GetAll();
+        Task<IEnumerable<TType>> GetAllAsync();
+        IQueryable<TType> GetAllAttached();
+        void Add(TType item);
+        Task AddAsync(TType item);
+        bool Delete(TType entity);
+        Task<bool> DeleteAsync(TType entity);
+        bool Update(TType item);
+        Task<bool> UpdateAsync(TType item);
+
+    }
+}
