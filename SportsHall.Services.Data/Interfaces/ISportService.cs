47c4bdb41ef03935936a36689f654b9a8ce37103
VioletaSotirova <villy_553@abv.bg>
2024-11-11 16:01:20 +0200

    Create Services.Data and SportService

---
+++ SportsHall.Services.Data/Interfaces/ISportService.cs
@@ -0,0 +1,9 @@
+﻿using SportsHall.Web.ViewModels;
+
+namespace SportsHall.Services.Data.Interfaces
+{
+    public interface ISportService
+    {
+        Task<IEnumerable<SportsViewModel>> GetAllAsync();
+    }
+}
---
+++ SportsHall.Services.Data/SportService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using SportsHall.Data.Repository.Interfaces;
+using SportsHall.Services.Data.Interfaces;
+using SportsHall.Web.ViewModels;
+using SportsHall.Data.Models;
+using SportsHall.Services.Mapping;
+
+namespace SportsHall.Services.Data
+{
+    public class SportService : ISportService
+    {
+        private IRepository<Sport, int> sportRepository;
+        public SportService(IRepository<Sport, int> sportRepository)
+        {
+            this.sportRepository = sportRepository;
+        }
+        public async Task<IEnumerable<SportsViewModel>> GetAllAsync()
+        {
+            var sports = await this.sportRepository
+                .GetAllAttached()
+                .To<SportsViewModel>()
+                .ToListAsync();
+
+            return sports;
+        }
+    }
+}
