32c66fec27148ece89c1d0f2cc6fa8de33bd36be
VioletaSotirova <villy_553@abv.bg>
2024-11-11 16:00:08 +0200

    Create migration AddSportDefaultImage

---
+++ SportsHall.Data/Migrations/20241105225753_AddSportDefaultImage.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportsHall.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSportDefaultImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Sports",
+                type: "nvarchar(2083)",
+                maxLength: 2083,
+                nullable: true,
+                defaultValue: "/images/defaultSportImage.jpg",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Sports",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2083)",
+                oldMaxLength: 2083,
+                oldNullable: true,
+                oldDefaultValue: "/images/defaultSportImage.jpg");
+        }
+    }
+}
