2ea7472b8c1c931a1f328dfaea21eb5d4c2a6f6b
VioletaSotirova <villy_553@abv.bg>
2024-11-19 16:08:17 +0200

    Create ViewModels

---
+++ SportsHall.Web.ViewModels/CoachDetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using SportsHall.Data.Models;
+using SportsHall.Services.Mapping;
+
+namespace SportsHall.Web.ViewModels
+{
+    public class CoachDetailsViewModel : IMapFrom<Coach>
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? Email { get; set; }
+        public string Phone { get; set; } = null!;
+        public string Expirience { get; set; } = null!;
+        public string? ImageUrl { get; set; }
+ 
+    }
+}
---
+++ SportsHall.Web.ViewModels/SportEditViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SportsHall.Data.Models;
+
+namespace SportsHall.Web.ViewModels
+{
+    public class SportEditViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Max participants must be greater than 0.")]
+        public int MaxParticipants { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        public IList<SelectListItem>? AvailableCoaches { get; set; } 
+
+        public List<int> SelectedCoaches { get; set; } = new List<int>();
+
+        public List<string> SelectedCoachesNames { get; set; } = new List<string>();
+    }
+}
