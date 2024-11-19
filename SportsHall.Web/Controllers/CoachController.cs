7fdbeeebef5a6ebb97a532a121139e7daeecf6f8
VioletaSotirova <villy_553@abv.bg>
2024-11-19 16:07:59 +0200

    Update CoachController and SportController

---
+++ SportsHall.Web/Controllers/CoachController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsHall.Data;
+using SportsHall.Services.Data.Interfaces;
+using SportsHall.Web.ViewModels;
+
+namespace SportsHall.Web.Controllers
+{
+    public class CoachController : Controller
+    {
+        private readonly ICoachService coachService;
+        private readonly SportsHallDbContext dbContext;
+        public CoachController(SportsHallDbContext dbContext, ICoachService coachService)
+        {
+            this.coachService = coachService;
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            IEnumerable<CoachesViewModel> coaches =
+               await this.coachService.GetAllAsync();
+
+            return View(coaches);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var coach = await this.coachService.DetailsAsync(id);
+            if (coach == null)
+            {
+                return NotFound();
+            }
+
+            return View(coach);
+        }
+    }
+}
--- SportsHall.Web/Controllers/SportController.cs
+++ SportsHall.Web/Controllers/SportController.cs
@@ -1,21 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-
+using SportsHall.Services.Data.Interfaces;
+using System.Data.Common;
 using SportsHall.Data;
 using SportsHall.Web.ViewModels;
-using SportsHall.Services.Data.Interfaces;
-using SportsHall.Services.Mapping;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace SportsHall.Web.Controllers
 {
     public class SportController : Controller
     {
-        private readonly SportsHallDbContext context;
         private readonly ISportService sportService;
-        public SportController(SportsHallDbContext context, ISportService sportService)
+        private readonly SportsHallDbContext dbContext;
+        private readonly ICoachService coachService;
+        public SportController(SportsHallDbContext dbContext, ISportService sportService, ICoachService coachService)
         {
-            this.context = context;
             this.sportService = sportService;
+            this.dbContext = dbContext;
+            this.coachService = coachService;
         }
 
         [HttpGet]
@@ -23,7 +25,7 @@ namespace SportsHall.Web.Controllers
         {
             IEnumerable<SportsViewModel> sports =
                 await this.sportService.GetAllAsync();
-                
+
             return View(sports);
         }
 
@@ -39,5 +41,39 @@ namespace SportsHall.Web.Controllers
             return View(sport);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var sport = await this.sportService.EditAsync(id);
+
+            if (sport == null)
+            {
+                return NotFound();
+            }
+
+            return View(sport);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(SportEditViewModel model, int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.AvailableCoaches = await coachService.GetAllCoachesAsSelectListAsync();
+
+                if (model.SelectedCoaches != null)
+                {
+                    model.SelectedCoachesNames = await coachService.GetCoachNamesByIdsAsync(model.SelectedCoaches);
+                }
+
+                return View(model);
+            }
+
+            await sportService.UpdateSportAsync(model);
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
+
