9d1abcf40331480796c22a605a9e7a8e80e7ae8d
VioletaSotirova <villy_553@abv.bg>
2024-11-29 00:03:15 +0200

    Add migration RemovingMappingTable

---
+++ SportsHall.Data/Migrations/20241124202234_RemovingMappingTable.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace SportsHall.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovingMappingTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SportsCoaches");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SportsCoaches",
+                columns: table => new
+                {
+                    CoachId = table.Column<int>(type: "int", nullable: false),
+                    SportId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SportsCoaches", x => new { x.CoachId, x.SportId });
+                    table.ForeignKey(
+                        name: "FK_SportsCoaches_Coaches_CoachId",
+                        column: x => x.CoachId,
+                        principalTable: "Coaches",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SportsCoaches_Sports_SportId",
+                        column: x => x.SportId,
+                        principalTable: "Sports",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "SportsCoaches",
+                columns: new[] { "CoachId", "SportId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 1, 2 },
+                    { 2, 4 },
+                    { 3, 3 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SportsCoaches_SportId",
+                table: "SportsCoaches",
+                column: "SportId");
+        }
+    }
+}
