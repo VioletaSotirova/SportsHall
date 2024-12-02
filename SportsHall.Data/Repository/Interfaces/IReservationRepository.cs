eec112fd2c0c4b9608bc98ec1cdf4c3a271caa78
VioletaSotirova <villy_553@abv.bg>
2024-12-02 06:01:20 +0200

    Create IReservasationRepository and ReservationRepository

---
+++ SportsHall.Data/Repository/Interfaces/IReservationRepository.cs
@@ -0,0 +1,10 @@
+﻿using SportsHall.Data.Models;
+
+namespace SportsHall.Data.Repository.Interfaces
+{
+    public interface IReservationRepository : IRepository<Reservation, int>
+    {
+        Task<Reservation> GetReservationAsync(int trainingId, string userId);
+        Task<IEnumerable<Reservation>> GetReservationsByUserIdAsync(string userId);
+    }
+}
---
+++ SportsHall.Data/Repository/ReservationRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsHall.Data.Models;
+using SportsHall.Data.Repository.Interfaces;
+
+namespace SportsHall.Data.Repository
+{
+    public class ReservationRepository : BaseRepository<Reservation, int>, IReservationRepository
+    {
+        public ReservationRepository(SportsHallDbContext dbContext) : base(dbContext)
+        {
+
+        }
+
+        public async Task<Reservation> GetReservationAsync(int trainingId, string userId)
+        {
+            var reservation = await dbContext.Reservations
+                .AsNoTracking()
+                 .FirstOrDefaultAsync(r => r.UserId == int.Parse(userId) && r.TrainingId == trainingId);
+
+            return reservation;
+        }
+        public async Task<IEnumerable<Reservation>> GetReservationsByUserIdAsync(string userId)
+        {
+            var reservations = await dbContext.Reservations
+                .AsNoTracking()
+               .Where(r => r.UserId == int.Parse(userId))
+               .Include(r => r.Training)
+               .ThenInclude(t => t.Sport)
+               .Include(r => r.Training.Coach)
+               .ToListAsync();
+
+            return reservations;
+        }
+    }
+}
