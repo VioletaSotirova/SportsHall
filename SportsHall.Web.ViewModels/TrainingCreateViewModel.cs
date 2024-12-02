d795576a8af31131adb0e52a25b9ca9a6367bc7d
VioletaSotirova <villy_553@abv.bg>
2024-12-02 06:03:33 +0200

    Change type from IEnumerable to ICollection

--- SportsHall.Web.ViewModels/TrainingCreateViewModel.cs
+++ SportsHall.Web.ViewModels/TrainingCreateViewModel.cs
@@ -1,20 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace SportsHall.Web.ViewModels
 {
     public class TrainingCreateViewModel
     {
         public int Id { get; set; }
+        [Range(1, int.MaxValue)]
         public int SportId { get; set; }
-        public IEnumerable<SelectListItem> Sports { get; set; } = new List<SelectListItem>();
+        public ICollection<SelectListItem> Sports { get; set; } = new List<SelectListItem>();
+        [Range(1, int.MaxValue)]
         public int CoachId { get; set; }
-        public IEnumerable<SelectListItem> Coaches { get; set; } = new List<SelectListItem>();
+        public ICollection<SelectListItem> Coaches { get; set; } = new List<SelectListItem>();
+        [Range(1, int.MaxValue)]
         public int TrainingStatusId { get; set; }
-        public IEnumerable<SelectListItem> TrainingStatuses { get; set; } = new List<SelectListItem>();
+        public ICollection<SelectListItem> TrainingStatuses { get; set; } = new List<SelectListItem>();
         public DateTime Start { get; set; }
         public string Location { get; set; } = string.Empty;
         public int Duration { get; set; }
-        public int AvailableSpot { get; set; }
     }
 }
 
